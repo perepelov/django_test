e7ea20d31ddc2b41f7d102253078f2dd87b90dfc
Perepelov Alexey <perepelov.a@mail.ru>
2017-09-03 20:57:34 +0300

    added * to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5pshvq8x@+x((9jgn8=m)#-5+$i$k5miu5gf4btn+6v_6v#1(m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
